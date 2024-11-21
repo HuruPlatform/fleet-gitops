aa5fe06269e470b89a6ed7996674b4c76ecc2887
Pradeep Chauhan <pradeep.chauhan@huru.co>
2024-11-21 10:25:28 +0530

    addon 8

--- lib/macos/scripts/macos-shared.sh
+++ lib/macos/scripts/macos-shared.sh
@@ -1,12 +1,11 @@
-#!/bin/zsh
 #echo "Allowing guests to connect to shared folders enables users to access selected shared folders and their contents from different computers on a network"
-/usr/bin/sudo /usr/sbin/sysadminctl -smbGuestAccess off
+/usr/bin/sudo /usr/sbin/sysadminctl -smbGuestAccess off -password -
 #echo "Power Nap on the device:"
 /usr/bin/sudo /usr/bin/pmset -a powernap 0
 #echo "Wake on  on the device:"
 /usr/bin/sudo /usr/bin/pmset -a womp 0
 #echo "setting Sudo Timeout to zero"
-echo 'Defaults timestamp_timeout=0' | sudo tee -a /etc/sudoers.d/CIS_54_sudoconfiguration
-/usr/bin/sudo /usr/sbin/chown -R root:wheel /etc/sudoers.d/
+echo 'Defaults timestamp_timeout=0' | sudo tee -a /etc/sudoers.d/CIS_54_sudoconfiguration 
+/usr/bin/sudo /usr/sbin/chown -R root:wheel /etc/sudoers.d/CIS_54_sudoconfiguration
 #echo "Hide IP Address in Safari"
 /usr/bin/sudo -u  /usr/bin/defaults write /Users//Library/Containers/com.apple.Safari/Data/Library/Preferences/com.apple.Safari WBSPrivacyProxyAvailabilityTraffic -int 130272
