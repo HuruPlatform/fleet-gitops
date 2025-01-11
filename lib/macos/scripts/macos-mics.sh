90c02dcf352cc83d9d538afab64de6881bd58246
Pradeep Chuahan <pradeep.chauhan@huru.co>
2025-01-11 13:56:31 +0530

    updatesmac os 15

--- lib/macos/scripts/macos-mics.sh
+++ lib/macos/scripts/macos-mics.sh
@@ -37,3 +37,69 @@ done
 ################################################
 #print_info "Ensure an administrator account cannot login to another user's active and locked session"
 sudo security authorizationdb write system.login.screensaver use-login-window-ui
+sudo security authorizationdb write system.login.screensaver authenticate-session-owner
+################################################
+# 5.2.6 Complex passwords must contain uppercase and lowercase letters
+###############################################
+#print_info "Set password policy: must contain uppercase and lowercase letters"
+sudo pwpolicy -n /Local/Default -setglobalpolicy "requiresMixedCase=1"
+################################################
+# 5.1.1 Secure Home Folders 
+################################################
+#print_info "Securing home folders"
+for user in $users_list; do
+    sudo chmod -R og-rwx /Users/"$user" 2> /dev/null
+done
+################################################
+# 3.3 Retain install.log for 365 or more days with no maximum sizes
+################################################
+#print_info "Retain install.log for 365 days"
+sudo sed -i.bu '$s/$/ ttl=365/' /etc/asl/com.apple.install
+#print_info "Set maximum size to 1G"
+sudo sed -i.bu 's/all_max=[0-9]*[mMgG]/all_max=1G/g' /etc/asl/com.apple.install
+################################################
+# 2.9 Disable Power Nap
+################################################
+#print_info "Disable Power Nap"
+sudo pmset -a powernap 0
+################################################
+# 3.1 Enable security auditing 
+################################################
+#print_info "Enable security auditing "
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.auditd.plist 2> /dev/null
+sudo cp /etc/security/audit_control.example /etc/security/audit_control
+
+################################################
+# 3.2 Configure Security Auditing Flags per local organizational requirements
+################################################
+#print_info "Set auditing flags to 'all'"
+sudo sed -i.bu "s/^flags:.*/flags:all/g" /etc/security/audit_control 2> /dev/null 
+################################################
+# 3.4 Ensure security auditing retention
+################################################
+#print_info "Set audit records expiration to 1 gigabyte"
+#sudo sed -i.bu "s/^expire-after:.*/expire-after:1G/g" /etc/security/audit_control
+sudo cp /etc/security/audit_control ./tmp.txt; origExpire=$(cat ./tmp.txt  | grep expire-after);  sed "s/{origExpire}/expire-after:60d OR 5G/" ./tmp.txt > /etc/security/audit_control; rm ./tmp.txt;
+################################################
+# 6.2 Turn on filename extensions
+################################################
+#print_info "Turn on filename extensions"
+for user in $users_list; do
+    sudo -u "$user" defaults write /Users/"$user"/Library/Prefjrences/.GlobalPreferences.plist AppleShowAllExtensions -bool true
+done
+################################################
+# 3.5 Control access to audit records
+################################################
+#print_info "Set the audit records to the root user and wheel group"
+sudo chown -R root:wheel /etc/security/audit_control 2> /dev/null
+sudo chmod -R -o-rw /etc/security/audit_control 2> /dev/null
+sudo chown -R root:wheel /var/audit/ 2> /dev/null
+sudo chmod -R -o-rw /var/audit/ 2> /dev/null
+################################################
+# 4.2 Enable "Show Wi-Fi/Bluetooth status in menu bar"
+################################################
+#print_info "Enable 'Show Wi-Fi/Bluetooth status in menu bar' for all users" 
+for user in $users_list; do
+    sudo -u "$user" defaults -currentHost write com.apple.controlcenter.plist WiFi -int 18
+    sudo -u "$user" defaults -currentHost write com.apple.controlcenter.plist Bluetooth -int 18
+done
