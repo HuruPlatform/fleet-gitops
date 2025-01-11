3bac85cd39bf1886fe0d737295eb61761a0b0b6d
Pradeep Chuahan <pradeep.chauhan@huru.co>
2025-01-11 13:17:31 +0530

    updatesmac os 15

--- lib/macos/scripts/macos-mics.sh
+++ lib/macos/scripts/macos-mics.sh
@@ -1,6 +1,8 @@
 ## Organisation info
 org="Huru Payments"
 org_contact="itsupport@huru.co"
+# List of current users
+users_list=$(dscacheutil -q user | grep -A 3 -B 2 -e uid:\ 5'[0-9][0-9]' | grep name | cut -d' ' -f2)
 ## Messages 
 login_screen_msg="If you found this laptop please let $org know at $org_contact. A rewards will be provided.\nSi vous trouvez cet ordinateur, veuillez s'il vous plait contacter $org à $org_contact. Une récomponse sera attribuée."
 login_window_banner="* * * * * * * * * * W A R N I N G * * * * * * * * * *
@@ -23,3 +25,15 @@ sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "
 #print_info "Add login window banner"
 echo "$login_window_banner" | sudo tee /Library/Security/PolicyBanner.txt
 sudo chmod 644 "/Library/Security/PolicyBanner."*  2> /dev/null
+################################################
+# 2.5.6 Limit Ad tracking and personalized Ads
+################################################
+#print_info "Limit Ad tracking and personalized Ads"
+for user in $users_list; do
+    sudo -u "$user" defaults -currentHost write /Users/"$user"/Library/Preferences/com.apple.Adlib.plist allowApplePersonalizedAdvertising -bool false 2> /dev/null
+done
+################################################
+# 5.12 Ensure an administrator account cannot login to another user's active and locked session
+################################################
+#print_info "Ensure an administrator account cannot login to another user's active and locked session"
+sudo security authorizationdb write system.login.screensaver use-login-window-ui
