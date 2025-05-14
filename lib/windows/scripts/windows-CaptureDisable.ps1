cdea57410fcada75c37cd87925e235c8afab137b
Pradeep Chuahan <pradeep.chauhan@huru.co>
2025-05-14 11:47:46 +0530

    hotkeydisabled

--- lib/windows/scripts/windows-CaptureDisable.ps1
+++ lib/windows/scripts/windows-CaptureDisable.ps1
@@ -5,12 +5,32 @@ $regPath2 = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 if (!(Test-Path $regPath)) {
     New-Item -Path $regPath -Force
 }
-
 # Set the DisableSnippingTool value
 New-ItemProperty -Path $regPath -Name "DisableSnippingTool" -Value 1 -PropertyType DWORD -Force
 # Set the DisableSnippingTool value to 0 to enable Snipping Tool
 
-New-ItemProperty -Path $regPath2  -Name "DisabledHotkeys"  -PropertyType String -Value "S" -Force
+
+
+# Create the registry path if it doesn't exist
+#if (-not (Test-Path -Path $regPath2)) {
+#    New-Item -Path $regPath2 -Force
+#}
+# Create or update the DisabledHotkeys value
+#New-ItemProperty -Path $regPath2  -Name "DisabledHotkeys"  -PropertyType String -Value "S" -Force
 
 #Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "DisabledHotkeys" -ErrorAction SilentlyContinue
 
+
+$users = Get-ChildItem -Path Registry::HKEY_USERS | Where-Object { $_.Name -match '^HKEY_USERS\\S+-\d+-\d+$' }
+
+foreach ($user in $users) {
+    $regPath = "$($user.Name)\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+
+    try {
+        New-Item -Path "Registry::$regPath" -Force | Out-Null
+        New-ItemProperty -Path "Registry::$regPath" -Name "DisabledHotkeys" -PropertyType String -Value "S" -Force
+        Write-Host "Set DisabledHotkeys for $($user.Name)"
+    } catch {
+        Write-Warning "Failed to set key for $($user.Name): $_"
+    }
+}
